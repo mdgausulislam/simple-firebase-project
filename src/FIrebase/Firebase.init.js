98af6d200dd1e4a4c0fb9f122311ef8acab6e5eb
unknown <gausulislam5@gmail.com>
2023-10-15 19:32:54 +0600

    update add firebase and update components nested router

---
+++ src/FIrebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpmxRoQDPuUIEmEsT16R7OgTHPqObApZM",
+  authDomain: "simple-firebase-project-c92cf.firebaseapp.com",
+  projectId: "simple-firebase-project-c92cf",
+  storageBucket: "simple-firebase-project-c92cf.appspot.com",
+  messagingSenderId: "170522660697",
+  appId: "1:170522660697:web:8146a746ae8129f255c648"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+1.visit: console.firebase.google.com
+2.create a project (skips google analytics)
+3. Register app (create config)
+4.npm install firebase
+5. add config file to your project
+6 DANGER: do not publish or make firebase config to public by pushing those by github
+*/
