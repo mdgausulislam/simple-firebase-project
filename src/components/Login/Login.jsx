9062e5f2841aeeec4f374c92ba67f0421012bf0f
unknown <gausulislam5@gmail.com>
2023-10-15 23:50:27 +0600

    update singin and sing out and show display user information

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import React, { useState } from 'react';
+import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth'
 import app from '../../FIrebase/Firebase.init';
 
 const Login = () => {
+    const [user,setUser]=useState(null);
     const auth=getAuth(app)
     const provider=new GoogleAuthProvider();
    
     const handleGoogleSignIn=()=>{
         signInWithPopup(auth,provider)
         .then(result=>{
-            const user=result.user;
+            const loggedInUser=result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
         })
         .catch(error=>{
             console.log('error:',error.message);
         })
     }
+
+    const handleSignOut=()=>{
+        signOut(auth)
+        .then(result=>{
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error=>{
+            console.log();
+        })
+    }
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google login</button>
+            {   user?
+                <button onClick={handleSignOut}>Sign Out</button>:
+                <button onClick={handleGoogleSignIn}>Google login</button>
+            }
+           { user && <div>
+                <h3>user:{user.displayName}</h3>
+                <p>Email:{user.email}</p>
+                <img src={user.photoURL} alt="" />
+            </div>} 
         </div>
     );
 };
