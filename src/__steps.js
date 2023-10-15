2fb19037b30194560d95ad3b5bd5bc5bc71fc62a
unknown <gausulislam5@gmail.com>
2023-10-15 22:55:37 +0600

    update google login successfully

--- src/__steps.js
+++ src/__steps.js
@@ -5,4 +5,11 @@
 4.npm install firebase
 5. add config file to your project
 6 DANGER: do not publish or make firebase config to public by pushing those by github
+7. go to docs > build > authentication >web>get started
+8. export app from the firebase.config.js file: export default app
+9.Login.jsx:import getAuth from firebase/auth
+10. create const auth=getAuth(app)
+11. import googleAuthProvider and create a new provider
+12.useSignINWithPopup and pass auth and provider
+13 activate sign-in method (google, facebook, github,etc...)
 */
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import app from '../../FIrebase/Firebase.init';
+
+const Login = () => {
+    const auth= getAuth(app);
+    const provider=new GoogleAuthProvider();
+
+
+    const handleGoogleSignIn=()=>{
+        signInWithPopup(auth,provider)
+        .then(result=>{
+            const user=result.user;
+            console.log(user);
+        })
+        .catch(error=>{
+            console.log('error:',error.message);
+        })
+    }
+    return (
+        <div>
+            <button onClick={handleGoogleSignIn}>Google login</button>
+        </div>
+    );
+};
+
+export default Login;
