cf29ae698187e2eb70eaa99a9d0e02a78ff704b1
unknown <gausulislam5@gmail.com>
2023-10-16 00:46:35 +0600

    update github login and display data show

--- src/__steps.js
+++ src/__steps.js
@@ -1,15 +1,31 @@
 /* 
+-----------------------------initial setup--------------------------
+------------------------------------------------------------------------
+
 1.visit: console.firebase.google.com
 2.create a project (skips google analytics)
 3. Register app (create config)
 4.npm install firebase
 5. add config file to your project
 6 DANGER: do not publish or make firebase config to public by pushing those by github
+
+--------------------------------Integration-------------------------
+---------------------------------------------------------------------
+
 7. go to docs > build > authentication >web>get started
 8. export app from the firebase.config.js file: export default app
 9.Login.jsx:import getAuth from firebase/auth
 10. create const auth=getAuth(app)
+
+---------------------------------Provider setup----------------------------
+----------------------------------------------------------------------------
+
 11. import googleAuthProvider and create a new provider
 12.useSignINWithPopup and pass auth and provider
 13 activate sign-in method (google, facebook, github,etc...)
+
+----------------------------------------more auth provider---------------
+--------------------------------------------------------------------------
+
+1.activate the auth provider (create app,provide, redirect url,client id, client secret)
 */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,45 +1,62 @@
 import React, { useState } from 'react';
-import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth'
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../FIrebase/Firebase.init';
 
 const Login = () => {
-    const [user,setUser]=useState(null);
-    const auth=getAuth(app)
-    const provider=new GoogleAuthProvider();
-   
-    const handleGoogleSignIn=()=>{
-        signInWithPopup(auth,provider)
+    const [user, setUser] = useState(null);
+    const auth = getAuth(app)
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log('error:', error.message);
+            })
+    }
+    const handleGithubSingIn=()=>{
+        signInWithPopup(auth,githubProvider)
         .then(result=>{
-            const loggedInUser=result.user;
-            console.log(loggedInUser);
-            setUser(loggedInUser);
+            const loggedUser=result.user;
+            console.log(loggedUser);
+            setUser(loggedUser)
+
         })
         .catch(error=>{
             console.log('error:',error.message);
         })
     }
 
-    const handleSignOut=()=>{
+    const handleSignOut = () => {
         signOut(auth)
-        .then(result=>{
-            console.log(result);
-            setUser(null);
-        })
-        .catch(error=>{
-            console.log();
-        })
+            .then(result => {
+                console.log(result);
+                setUser(null);
+            })
+            .catch(error => {
+                console.log();
+            })
     }
     return (
         <div>
-            {   user?
-                <button onClick={handleSignOut}>Sign Out</button>:
-                <button onClick={handleGoogleSignIn}>Google login</button>
+            {user ?
+                <button onClick={handleSignOut}>Sign Out</button> :
+                <div>
+                    <button onClick={handleGoogleSignIn}>Google login</button>
+                    <button onClick={handleGithubSingIn}>Github Login</button>
+                </div>
+
             }
-           { user && <div>
+            {user && <div>
                 <h3>user:{user.displayName}</h3>
                 <p>Email:{user.email}</p>
                 <img src={user.photoURL} alt="" />
-            </div>} 
+            </div>}
         </div>
     );
 };
